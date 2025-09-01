aa9b54f36286d437ef71918797548a3fce38556b
Henrique Custódia <martinshenrique8@gmail.com>
2025-09-01 10:19:54 -0300

    feat: utilizando loggedInUserStoreService.isLoggedIn() par amostrar o botão de logout

--- src/app/core/layout/components/header/header.ts
+++ src/app/core/layout/components/header/header.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { LogoutFacadeService } from '../../../auth/facades/logout-facade.service';
 import { Router } from '@angular/router';
+import { LoggedInUserStoreService } from '../../../auth/stores/logged-in-user-store';
 
 @Component({
   selector: 'app-header',
@@ -12,8 +13,11 @@ import { Router } from '@angular/router';
 })
 export class Header {
 
-  logoutFacadeService = inject(LogoutFacadeService);
-  router = inject(Router);
+  private readonly logoutFacadeService = inject(LogoutFacadeService);
+  private readonly router = inject(Router);
+  private readonly loggedInUserStoreService = inject(LoggedInUserStoreService)
+
+  isLoggedIn = computed(() => this.loggedInUserStoreService.isLoggedIn())
 
   logout() {
     this.logoutFacadeService.logout()
