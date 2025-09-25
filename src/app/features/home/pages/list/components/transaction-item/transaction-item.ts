1d05ab17e77c13d168355f3cb71a9ffced937662
Henrique Custódia <martinshenrique8@gmail.com>
2025-09-25 18:58:41 -0300

    feat: adiciona diretivas estruturais

--- src/app/features/home/pages/list/components/transaction-item/transaction-item.ts
+++ src/app/features/home/pages/list/components/transaction-item/transaction-item.ts
@@ -1,21 +1,28 @@
 import { Component, input, output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from "@angular/material/card";
-import { TransactionValue } from "./components/transaction-value/transaction-value";
+import { MatCardModule } from '@angular/material/card';
+import { TransactionValue } from './components/transaction-value/transaction-value';
 import { Transaction } from '@shared/transaction/interfaces/transaction';
 import { CustomColorDirective } from '@shared/material/buttons/directives/custom-color.directive';
+import { IsIncomeDirective } from './directives/is-income.directive';
+import { MatChipsModule } from '@angular/material/chips';
 
 @Component({
   selector: 'app-transaction-item',
-  imports: [MatCardModule, MatButtonModule, TransactionValue, CustomColorDirective],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    TransactionValue,
+    CustomColorDirective,
+    IsIncomeDirective,
+    MatChipsModule
+  ],
   templateUrl: './transaction-item.html',
-  styleUrl: './transaction-item.scss'
+  styleUrl: './transaction-item.scss',
 })
 export class TransactionItem {
-
   transaction = input.required<Transaction>();
 
   edit = output<Transaction>();
   remove = output<Transaction>();
-
 }
