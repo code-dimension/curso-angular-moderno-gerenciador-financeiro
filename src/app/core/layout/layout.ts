a168bee5b3ceda4f22d33081158c4048e298d5eb
Henrique Custódia <martinshenrique8@gmail.com>
2025-10-11 18:24:56 -0300

    feat: adiciona items para sidenav

--- src/app/core/layout/layout.ts
+++ src/app/core/layout/layout.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { Header } from './components/header/header';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { IsUrlActivePipe } from './pipes/is-url-active-pipe';
 
 @Component({
   selector: 'app-layout',
-  imports: [Header, RouterOutlet, MatSidenavModule],
+  imports: [
+    Header,
+    RouterOutlet,
+    MatSidenavModule,
+    MatListModule,
+    RouterLink,
+    IsUrlActivePipe,
+  ],
   templateUrl: './layout.html',
-  styleUrl: './layout.scss'
+  styleUrl: './layout.scss',
 })
 export class Layout {
-
+  links = signal([
+    {
+      label: 'Home',
+      url: '/',
+    },
+  ]);
 }
