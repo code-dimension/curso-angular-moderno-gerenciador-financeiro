6083af82a17683cde74ed8fd58be0a5aea27e211
Henrique Custódia <martinshenrique8@gmail.com>
2025-10-13 12:56:43 -0300

    fix: cria componente sidenav-items

--- src/app/core/layout/layout.ts
+++ src/app/core/layout/layout.ts
@@ -1,31 +1,13 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { Header } from './components/header/header';
-import {
-  RouterLink,
-  RouterLinkActive,
-  RouterOutlet,
-} from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatListModule } from '@angular/material/list';
+import { SidenavItemsComponent } from './components/sidenav-items/sidenav-items.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [
-    Header,
-    RouterOutlet,
-    MatSidenavModule,
-    MatListModule,
-    RouterLink,
-    RouterLinkActive,
-  ],
+  imports: [Header, RouterOutlet, MatSidenavModule, SidenavItemsComponent],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
-export class Layout {
-  links = signal([
-    {
-      label: 'Home',
-      url: '/',
-    },
-  ]);
-}
+export class Layout {}
