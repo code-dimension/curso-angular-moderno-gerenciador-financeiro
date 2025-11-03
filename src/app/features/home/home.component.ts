ee11ed79526bb3a980eacf3d4b1a2d6c7acc34a8
Henrique Custódia <martinshenrique8@gmail.com>
2025-11-03 13:48:43 -0300

    feat: criando gráfico de exemplo

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { Balance } from './components/balance/balance';
 import { Transaction } from '@shared/transaction/interfaces/transaction';
+import { PieChartComponent } from './components/pie-chart/pie-chart.component';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance],
+  imports: [Balance, PieChartComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
