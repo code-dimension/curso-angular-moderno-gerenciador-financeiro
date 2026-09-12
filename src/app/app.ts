7bd3145730b70070aafb4374c1a25f39924bb498
Henrique Custódia <martinshenrique8@gmail.com>
2026-09-12 15:33:23 -0300

    refactor: executa self-closing schematic

--- src/app/app.ts
+++ src/app/app.ts
@@ -6,7 +6,6 @@ import { RouterOutlet } from '@angular/router';
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
   protected title = 'gerenciador-financeiro';
