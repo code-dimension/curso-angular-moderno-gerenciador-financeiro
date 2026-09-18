e533aedac4782e6057bfeb600726edd0839c6576
Henrique Custódia <martinshenrique8@gmail.com>
2026-09-18 18:01:25 -0300

    feat: utiliza a função isActive de Angular Router

--- src/app/core/layout/components/sidenav/components/sidenav-items/sidenav-items.component.ts
+++ src/app/core/layout/components/sidenav/components/sidenav-items/sidenav-items.component.ts
@@ -1,31 +1,51 @@
-import { ChangeDetectionStrategy, Component, computed, inject, signal } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  signal,
+} from '@angular/core';
+import {
+  isActive,
+  Router,
+  RouterLink,
+  RouterLinkActive,
+} from '@angular/router';
 import { MatListModule } from '@angular/material/list';
 import { LogoutDirective } from './directives/logout.directive';
 import { LoggedInUserStoreService } from '@core/auth/stores/logged-in-user-store';
 import { SidenavVisibilityStore } from '@core/layout/stores/sidenav-visibility.store';
+
+function isRouteActive(route: string) {
+  const router = inject(Router);
+
+  return isActive(route, router, {
+    paths: 'exact',
+  });
+}
+
+function createSidenavItem({ label, url }: { label: string; url: string }) {
+  return {
+    label,
+    url,
+    isActive: isRouteActive(url),
+  };
+}
 
 @Component({
   selector: 'app-sidenav-items',
   imports: [RouterLink, RouterLinkActive, MatListModule, LogoutDirective],
   templateUrl: './sidenav-items.component.html',
   styleUrl: './sidenav-items.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush
-
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SidenavItemsComponent {
   private readonly loggedInUserStoreService = inject(LoggedInUserStoreService);
   private readonly sidenavVisibilityStore = inject(SidenavVisibilityStore);
 
   links = signal([
-    {
-      label: 'Home',
-      url: '/',
-    },
-     {
-      label: 'Transações',
-      url: '/transactions',
-    },
+    createSidenavItem({ label: 'Dashboard', url: '/' }),
+    createSidenavItem({ label: 'Transações', url: '/transactions' }),
   ]);
 
   isLoggedIn = computed(() => this.loggedInUserStoreService.isLoggedIn());
